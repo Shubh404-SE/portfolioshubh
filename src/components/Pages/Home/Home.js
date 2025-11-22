55aab6c329d557e036ee055170740a3ef1510221
Shubh404-SE <tanwarshubham29@gmail.com>
2025-11-22 14:18:30 +0530

    double nav & footer issue resolved

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -5,13 +5,10 @@ import Skill from "../Skill_Service/Skill";
 import ProjectDetail from "../Project/Projects";
 import Contact from "../Contact/Contact";
 import HeroSection from "../../HeroSection/HeroSection";
-import Navbar from "../../Navbar/Navbar";
-import Footer from "../../Footer/Footer";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <HeroSection />
       <About />
       <Skill />
@@ -19,7 +16,6 @@ export default function Home() {
       <ProjectDetail />
       <Education />
       <Contact />
-      <Footer />
     </>
   );
 }
